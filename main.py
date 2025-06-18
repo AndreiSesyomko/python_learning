07b8b9094a797eb4f0ee6966e3d235e1b696045c
Andrei Sesyomko <andreinev007@gmail.com>
2025-06-18 10:10:10 +0300

    finished #2

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 
-import string
-
-def is_palindrome(target):
-    target = target.replace(' ', '').lower()
+def is_palindrome(target_string):
+    target_string = target_string.replace(' ', '').lower()
     result = ""
-    for i in range(0, len(target)):
-        if target[i].isdigit() or target[i].isalpha():
-            result += target[i]
+    for i in range(0, len(target_string)):
+        if target_string[i].isdigit() or target_string[i].isalpha():
+            result += target_string[i]
     for i in range(0, int(len(result)/2)):
         if result[i] != result[-(i+1)]:
             return False
     return True
 
-target = input("Enter a string: ")
-print(is_palindrome(target))
+def coincidence(target_list=None, target_range=None):
+    if target_range is None or target_list is None:
+        return []
+    result = []
+    for item in target_list:
+        if not item is None and (isinstance(item, int) or isinstance(item, float)):
+            if target_range.start <= item < target_range.stop:
+                result.append(item)
+    return result
+
+print(coincidence([1, 2, 3, 4, 5], range(3, 6)))
+print(coincidence())
+print(coincidence([None, 1, 'foo', 4, 2, 2.5], range(1, 4)))
